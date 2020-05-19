f32c3f34d10ec37a51352e3a518e7be211ab08ad
shrawan03 <shrawank039@gmail.com>
2020-05-19 22:52:37 +0530

    Initial commit

---
+++ YuddhamGaming/app/src/main/java/com/matrixdeveloper/yuddhamgaming/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,43 @@
+package com.matrixdeveloper.yuddhamgaming.ui.main;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.matrixdeveloper.yuddhamgaming.R;
+
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.upcoming, R.string.ongoing, R.string.my_tournaments};
+    private final Context mContext;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        // Return a PlaceholderFragment (defined as a static inner class below).
+        return PlaceholderFragment.newInstance(position + 1);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mContext.getResources().getString(TAB_TITLES[position]);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+}
