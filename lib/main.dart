6af70944371b24912417f71a6b2ccb6c11798e88
Microinstance <partha.mcr@gmail.com>
2020-06-01 13:13:53 +0530

    Login check added in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,30 @@ import 'package:flutter/material.dart';
 import 'Auth/Login.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 
+import 'Dashboard/Dashboard.dart';
+import 'ServerAPI.dart';
+
 void main() {
   runApp(MyApp());
   OneSignal.shared.init("0ff4329e-5eda-4e47-8de0-0e4f05fb6f50");
   OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _isLogin();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,4 +40,15 @@ class MyApp extends StatelessWidget {
       home: Login(),
     );
   }
+
+  _isLogin() async {
+    if(await ServerAPI().isLogin()){
+      print("login");
+      Route route = MaterialPageRoute(builder: (context) => Dashboard());
+      Navigator.pushReplacement(context, route);
+    } else {
+      print("not login");
+    }
+  }
+
 }
